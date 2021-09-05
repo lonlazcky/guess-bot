18d27f71e8fe1ca748c6d9858c1ee0aa9362064a
lonlazcky <75757656+lonlazcky@users.noreply.github.com>
2021-09-05 21:16:47 +0200

    Add files via upload
    main still doesnt use this also its probably full of bugs

--- gametypes.py
+++ gametypes.py
@@ -1,3 +1,9 @@
+gameAnswers = [
+
+    "classicGameWon": "Good job, you guessed all the letters and the word was:\n`{0}`" #format: word
+
+]
+
 class game:
 	@staticmethod
 	def getRandomWord(number=1):
@@ -7,9 +13,8 @@ class game:
 
 		return text[2:-2]  #remove [""]
 
-	def revealLetter(self, letter):
-		word = self.word
-		revealedWord = self.revealedWord
+	def revealLetter(word, word_guessed_so_far, letter):
+		revealedWord = word_guessed_so_far
 		newRevealed = ""
 		wasLetterGuessed = False
 
@@ -26,23 +31,29 @@ class game:
 				newRevealed += revealedWordLetter
 				continue
 
+            #if letter was correctly guessed
 			wasLetterGuessed = True
 			newRevealed += letter
 
-		self.revealedWord = newRevealed
 		return wasLetterGuessed, newRevealed
-
-	def checkForWin(self):
-		return self.word == self.revealedWord
-
-	def win(self, message):
-		del games[message.channel.id]
-
-		return responses["WordGuessed"].format(
-			message.author.mention, self.word)
 
 class classic(game):
 	def __init__(self, channel):
 		self.channel = channel
 		self.word = super().getRandomWord()
 		self.revealedWord = "-" * len(self.word)
+
+	async def end(self):
+	    await self.channel.send(
+	        gameAnswers["classicGameWon"].format(self.word))
+
+    async def makeGuess(letter):
+        wasLetterGuessed, newRevealedWord = super().revealLetter(
+                                        self.word, self.revealedWord, letter)
+        if wasLetterGuessed:
+            self.revealedWord = newRevealedWord
+            await self.channel.send(
+                gameAnswers["classicLetterGuessed"].format())
+
+    def _is_message_guess():
+        pass
